45c51a52b1a09c78326b12ca9519da9df6377063
Sergio <sergio.mauz88@gmail.com>
2020-03-05 12:12:11 -0500

    Avoided more than 3 levels nested

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -2,20 +2,19 @@ def print_array(arr)
   i = 0
   s = ""
   while i < arr.length
-    s = s + " " + arr[i].to_s
+    s = s + ' ' + arr[i].to_s
     i += 1
   end
-  puts (s + "\n")
+  puts s
 end
+
 def bubble_sort(arr)
   if arr.length > 1
     k = arr.length
     while k > 1
       i = 0
       while i < k - 1
-        if arr[i + 1] < arr[i]
-          arr[i], arr[i + 1] = arr[i + 1], arr[i]
-        end
+        arr[i], arr[i + 1] = arr[i + 1], arr[i] if arr[i + 1] < arr[i]
         i += 1
       end
       k -= 1
@@ -23,5 +22,6 @@ def bubble_sort(arr)
   end
   print_array(arr)
 end
+
 array_a = [4, 3, 78, 2, 0, 2]
 bubble_sort(array_a)
