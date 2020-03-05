f41950892627d3397491419b734b4fcf524186b0
Sergio <sergio.mauz88@gmail.com>
2020-03-05 11:52:34 -0500

    Fixed mistakes on bubble-sort file

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,30 +1,28 @@
-def print_array (arr)
-    i = 0
-    s = ""
-    while i < arr.length
-      s = s + " " + arr[i].to_s
-      i = i + 1
-    end
-    puts (s + "\n")
+def print_array(arr)
+  i = 0
+  s = ""
+  while i < arr.length
+    s = s + " " + arr[i].to_s
+    i += 1
   end
-  
-  def bubble_sort (arr)
-    if arr.length > 1
-      k = arr.length
-      while k > 1 do
-        i = 0
-        while (i < k - 1) do
-          if arr[i + 1] < arr[i]
-            arr[i], arr[i + 1] = arr[i + 1], arr[i]
-          end
-          i = i + 1
+  puts (s + "\n")
+end
+def bubble_sort(arr)
+  if arr.length > 1
+    k = arr.length
+    while k > 1
+      i = 0
+      while i < k - 1
+        if arr[i + 1] < arr[i]
+          arr[i], arr[i + 1] = arr[i + 1], arr[i]
         end
-        k = k - 1
+        i += 1
       end
+      k -= 1
     end
-  
-    print_array(arr)
   end
-  
-  array_a = [4,3,78,2,0,2]
-  bubble_sort(array_a)
+  print_array(arr)
+end
+
+array_a = [4, 3, 78, 2, 0, 2]
+bubble_sort(array_a)
