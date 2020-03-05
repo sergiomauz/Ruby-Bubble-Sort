8c050ae950b54d096147d6799bb716765eaa9f70
peterrobert <pwambui93@gmail.com>
2020-03-05 18:53:46 +0300

    changed from four to two spaces

--- bubble-sort-by.rb
+++ bubble-sort-by.rb
@@ -1,34 +1,34 @@
 def print_array (arr)
-    i = 0
-    s = ""
-    while i < arr.length
-        s = s + " " + arr[i].to_s
-        i = i + 1
-    end
-    puts (s + "\n")
+  i = 0
+  s = ""
+  while i < arr.length
+    s = s + " " + arr[i].to_s
+    i = i + 1
+  end
+  puts (s + "\n")
 end
 
-def bubble_sort_by (arr)    
-    if arr.length > 1
-        k = arr.length    
-        while k > 1 do                        
-            i = 0            
-            while (i < k - 1) do
-                if yield(arr[i], arr[i + 1]) > 0
-                    arr[i], arr[i + 1] = arr[i + 1], arr[i]
-                end
-                i = i + 1                
-            end                                    
-            k = k - 1            
+def bubble_sort_by (arr)  
+  if arr.length > 1
+   k = arr.length  
+  while k > 1 do
+      i = 0 
+      while (i < k - 1) do
+        if yield(arr[i], arr[i + 1]) > 0
+          arr[i], arr[i + 1] = arr[i + 1], arr[i]
         end
-    end    
+        i = i + 1
+      end
+      k = k - 1
+    end
+  end  
 
-    return arr
+  return arr
 end
 
 
 new_array = bubble_sort_by(["hi","hello","hey", "h"]) do |left, right|
-    left.length - right.length
+  left.length - right.length
 end
 
 puts new_array
