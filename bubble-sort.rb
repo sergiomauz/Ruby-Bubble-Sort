830c9f6d1f32ce750263acb21922f8f34e4efd7c
Sergio <sergio.mauz88@gmail.com>
2020-03-05 10:17:57 -0500

    Fixed bubble sort code

---
+++ bubble-sort.rb
@@ -0,0 +1,30 @@
+def print_array (arr)
+    i = 0
+    s = ""
+    while i < arr.length
+        s = s + " " + arr[i].to_s
+        i = i + 1
+    end
+    puts (s + "\n")
+end
+
+def bubble_sort (arr)    
+    if arr.length > 1
+        k = arr.length     
+        while k > 1 do                        
+            i = 0            
+            while (i < k - 1) do
+                if arr[i + 1] < arr[i]                    
+                    arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                end
+                i = i + 1                
+            end                                     
+            k = k - 1            
+        end
+    end    
+
+    print_array(arr)
+end
+
+array_a = [4,3,78,2,0,2]
+bubble_sort(array_a)
