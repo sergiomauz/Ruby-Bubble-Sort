cd02d93c050f61b2c3dfd1c6f391ab4898e0444b
Sergio <sergio.mauz88@gmail.com>
2020-03-06 09:53:44 -0500

    Fixed complexity of bubble sort

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,15 +1,12 @@
-def bubble_sort(arr)
-  if arr.length > 1
-    k = arr.length
-    while k > 1
-      i = 0
-      while i < k - 1
-        arr[i], arr[i + 1] = arr[i + 1], arr[i] if arr[i + 1] < arr[i]
-        i += 1
-      end
-      k -= 1
+def bubble_sort(arr)  
+  k = arr.length
+  while k > 1
+    i = 0
+    while i < k - 1
+      arr[i], arr[i + 1] = arr[i + 1], arr[i] if arr[i + 1] < arr[i]
+      i += 1
     end
-  end
+    k -= 1
+    end
   return arr
 end
-
--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -1,26 +1,14 @@
-def print_array(arr)
-  i = 0
-  s = ''
-  while i < arr.length
-    s = s + ' ' + arr[i].to_s
-    i += 1
-  end
-  puts(s + "\n")
-end
-
-def bubble_sort_by(arr)
-  if arr.length > 1
-    k = arr.length
-    while k > 1
-      i = 0
-      while i < k - 1
-        if yield(arr[i], arr[i + 1]).positive?
-          arr[i], arr[i + 1] = arr[i + 1], arr[i]
-        end
-        i += 1
+def bubble_sort_by(arr)  
+  k = arr.length
+  while k > 1
+    i = 0
+    while i < k - 1
+      if yield(arr[i], arr[i + 1]).positive?
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
       end
-      k -= 1
+      i += 1
     end
-  end
-  arr
+    k -= 1
+  end  
+  return arr
 end
